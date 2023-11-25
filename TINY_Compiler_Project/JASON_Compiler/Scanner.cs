b15a401b4e2f79522131ddc54cf6ad43ae65e0eb
Farouk Khaled <faroukkhaled12350@gmail.com>
2023-11-25 22:37:52 +0200

    Tiny Language Template

---
+++ TINY_Compiler_Project/JASON_Compiler/Scanner.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public enum Token_Class
+{
+    Int, Float, String , Read, Write, Repeat, Until, If , ElseIf, Else , Then, Return , Endl,
+
+    PlusOp, MinusOp, MultiplyOp, DivideOp,
+
+    LessThanOp, GreaterThanOp, NotEqualOp, IsEqualOp,
+
+    AndOp, OrOp,
+
+    LParanthesisOp, RParanthesisOp,  LBrackectOp, RBracketOp,
+
+    AssignmentOp, SemicolonOp, CommaOp,
+
+    Idenifier, Number
+}
+
+namespace TINY_Compiler
+{
+    
+
+    public class Token
+    {
+       public string lex;
+       public Token_Class token_type;
+    }
+
+    public class Scanner
+    {
+        public List<Token> Tokens = new List<Token>();
+        Dictionary<string, Token_Class> ReservedWords = new Dictionary<string, Token_Class>();
+        Dictionary<string, Token_Class> Operators = new Dictionary<string, Token_Class>();
+
+        public Scanner()
+        {
+            ReservedWords.Add("int", Token_Class.Int);
+            ReservedWords.Add("float", Token_Class.Float);
+            ReservedWords.Add("string", Token_Class.String);
+            ReservedWords.Add("read", Token_Class.Read);
+            ReservedWords.Add("write", Token_Class.Write);
+            ReservedWords.Add("Repeat", Token_Class.Repeat);
+            ReservedWords.Add("until", Token_Class.Until);
+            ReservedWords.Add("if", Token_Class.If);
+            ReservedWords.Add("elseif", Token_Class.ElseIf);
+            ReservedWords.Add("else", Token_Class.Else);
+            ReservedWords.Add("then", Token_Class.Then);
+            ReservedWords.Add("return", Token_Class.Return);
+            ReservedWords.Add("endl", Token_Class.Endl);
+
+            
+            Operators.Add(";", Token_Class.SemicolonOp);
+            Operators.Add(",", Token_Class.CommaOp);
+            Operators.Add("(", Token_Class.LParanthesisOp);
+            Operators.Add(")", Token_Class.RParanthesisOp);
+            Operators.Add("=", Token_Class.IsEqualOp);
+            Operators.Add("<", Token_Class.LessThanOp);
+            Operators.Add(">", Token_Class.GreaterThanOp);
+            Operators.Add("<>", Token_Class.NotEqualOp);
+            Operators.Add("+", Token_Class.PlusOp);
+            Operators.Add("-", Token_Class.MinusOp);
+            Operators.Add("*", Token_Class.MultiplyOp);
+            Operators.Add("/", Token_Class.DivideOp);
+            Operators.Add("&&", Token_Class.AndOp);
+            Operators.Add("||", Token_Class.OrOp);
+            Operators.Add(":=", Token_Class.AssignmentOp);
+            Operators.Add("{", Token_Class.LBrackectOp);
+            Operators.Add("}", Token_Class.RBracketOp);
+
+        }
+
+        public void StartScanning(string SourceCode)
+        {
+            for(int i=0; i<SourceCode.Length;i++)
+            {
+                int j = i;
+                char CurrentChar = SourceCode[i];
+                string CurrentLexeme = CurrentChar.ToString();
+
+                if (CurrentChar == ' ' || CurrentChar == '\r' || CurrentChar == '\n')
+                    continue;
+
+                if (CurrentChar >= 'A' && CurrentChar <= 'z') //if you read a character
+                {
+                   
+                }
+
+                else if(CurrentChar >= '0' && CurrentChar <= '9')
+                {
+                   
+                }
+                else if(CurrentChar == '{')
+                {
+                   
+                }
+                else
+                {
+                   
+                }
+            }
+            
+            TINY_Compiler.TokenStream = Tokens;
+        }
+        void FindTokenClass(string Lex)
+        {
+            Token_Class TC;
+            Token Tok = new Token();
+            Tok.lex = Lex;
+            //Is it a reserved word?
+            
+
+            //Is it an identifier?
+            
+
+            //Is it a number?
+
+            //Is it an operator?
+
+            //Is it an undefined?
+        }
+
+    
+
+        bool isIdentifier(string lex)
+        {
+            bool isValid=true;
+            // Check if the lex is an identifier or not.
+            
+            return isValid;
+        }
+        bool isNumber(string lex)
+        {
+            bool isValid = true;
+            // Check if the lex is a Number (constant) or not.
+
+            return isValid;
+        }
+    }
+}
