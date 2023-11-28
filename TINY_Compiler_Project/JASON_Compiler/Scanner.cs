1a35a4e34d8b11a2365e40da28b18d7119ebf521
Badr <mohammadhani912@gmail.com>
2023-11-28 03:14:37 +0200

    added functions

--- TINY_Compiler_Project/JASON_Compiler/Scanner.cs
+++ TINY_Compiler_Project/JASON_Compiler/Scanner.cs
@@ -22,7 +22,9 @@ public enum Token_Class
 
     AssignmentOp, SemicolonOp, CommaOp,
 
-    Idenifier, Number
+    Idenifier, Number,
+
+    NewLine, Space
 }
 
 namespace TINY_Compiler
@@ -48,7 +50,7 @@ namespace TINY_Compiler
             ReservedWords.Add("string", Token_Class.DataTypeString);
             ReservedWords.Add("read", Token_Class.Read);
             ReservedWords.Add("write", Token_Class.Write);
-            ReservedWords.Add("Repeat", Token_Class.Repeat);
+            ReservedWords.Add("repeat", Token_Class.Repeat);
             ReservedWords.Add("until", Token_Class.Until);
             ReservedWords.Add("if", Token_Class.If);
             ReservedWords.Add("elseif", Token_Class.ElseIf);
@@ -75,6 +77,8 @@ namespace TINY_Compiler
             Operators.Add(":=", Token_Class.AssignmentOp); //done
             Operators.Add("{", Token_Class.LBrackectOp); // done
             Operators.Add("}", Token_Class.RBracketOp);  // done
+            Operators.Add("\n", Token_Class.NewLine);  // done
+            Operators.Add(" ", Token_Class.Space);  // done
         }
         
         public void StartScanning(string SourceCode)
@@ -88,28 +92,28 @@ namespace TINY_Compiler
                 if (CurrentChar == ' ' || CurrentChar == '\r' || CurrentChar == '\n')
                     continue;
 
-                if (CurrentChar >= 'A' && CurrentChar <= 'z') //if you read a character
-                {
-                   
-                }
 
-                else if(CurrentChar >= '0' && CurrentChar <= '9') // if yoy read number
+                //number
+                else if(CurrentChar >= '0' && CurrentChar <= '9') // if you read number
                 {
                     for(j = j + 1; j < SourceCode.Length; j++)
                     {
                         CurrentChar = SourceCode[j];
-                        if((CurrentChar >= '0' && CurrentChar <= '9') || CurrentChar == '.')
-                        {
-                            CurrentLexeme += CurrentChar.ToString();
-                        }
-                        else
+                         if(Operators.ContainsKey(CurrentChar.ToString()))
                         {
                             break;
+                        }
+                        else 
+                        {
+                            CurrentLexeme += CurrentChar.ToString();
+
                         }
                     }
                     i = j - 1;
                     FindTokenClass(CurrentLexeme);
                 }
+
+                //string
                 else if (CurrentChar == '"')
                 {
                     for (j = j + 1; j < SourceCode.Length; j++)
@@ -126,6 +130,40 @@ namespace TINY_Compiler
                     i = j - 1;
                     FindTokenClass(CurrentLexeme);
                 }
+
+
+                //comment
+                else if (CurrentChar == '/')
+                {
+
+                    if (i + 1 < SourceCode.Length && SourceCode[i + 1] == '*')
+                    {
+                        CurrentLexeme += SourceCode[i + 1].ToString();
+                        for (j = j + 2; j < SourceCode.Length; j++)
+                        {
+                            CurrentChar = SourceCode[j];
+                            if (CurrentChar == '*')
+                            {
+                                if (j + 1 < SourceCode.Length && SourceCode[j + 1] == '/')
+                                {
+                                    CurrentLexeme += CurrentChar.ToString();
+                                    CurrentChar = '/';
+                                    CurrentLexeme += CurrentChar.ToString();
+                                    j++;
+                                    break;
+                                }
+                            }
+                            CurrentLexeme += CurrentChar.ToString();
+
+                        }
+                    }
+                    i = j;
+                    FindTokenClass(CurrentLexeme);
+                }
+
+            
+
+
 
                 // if you read operator
                 else if(CurrentChar == '{' || CurrentChar == '}' 
@@ -196,53 +234,102 @@ namespace TINY_Compiler
               
                     FindTokenClass(CurrentLexeme);
                 }
-                //
 
+
+
+                //identifiers
+                else if ((CurrentChar >= 'a' && CurrentChar <= 'z') || (CurrentChar >= 'A' && CurrentChar <= 'Z'))
+                {
+
+                    for (j = j + 1; j < SourceCode.Length; j++)
+                    {
+                        CurrentChar = SourceCode[j];
+                        if (!((CurrentChar >= 'a' && CurrentChar <= 'z') || (CurrentChar >= 'A' && CurrentChar <= 'Z') || (CurrentChar >= '0' && CurrentChar <= '9')))
+                        {
+                            break;
+                        }
+
+                        CurrentLexeme += CurrentChar.ToString();
+                    } 
+
+                    i = j - 1;
+                    FindTokenClass(CurrentLexeme);
+                }
+
+
+                //None
                 else
                 {
                     FindTokenClass(CurrentChar.ToString());
                 }
             }
+
             
             TINY_Compiler.TokenStream = Tokens;
         }
+
+
         void FindTokenClass(string Lex)
         {
             Token_Class TC;
             Token Tok = new Token();
             Tok.lex = Lex;
+
             //Is it a string
             if (isString(Lex))
             {
-
                 Tok.token_type = Token_Class.String;
                 Tokens.Add(Tok);
             }
+
             //Is it a reserved word?
+            else if (isReservedkeyword(Lex))
+            {
+                Tok.token_type = ReservedWords[Lex];
+                Tokens.Add(Tok); 
+            }
 
 
             //Is it an identifier?
+            else if (isIdentifier(Lex))
+            {
+                Tok.token_type = Token_Class.Idenifier;
+                Tokens.Add(Tok);
+            }
 
+            //Is it a comment?
+            else if (isCommentStatment(Lex))
+            {
+                Tok.token_type = Token_Class.Comment;
+                Tokens.Add(Tok);
+            }
 
             //Is it a number?
-            if (isNumber(Lex))
+            else if (isNumber(Lex))
             {
                 Tok.token_type = Token_Class.Number;
                 Tokens.Add(Tok);
             }
+
             //Is it an operator?
-            if (isOperator(Lex))
+            else if (isOperator(Lex))
             {
                 Tok.token_type = Operators[Lex];
                 Tokens.Add(Tok);
             }
+
             //Is it an undefined?
-            //Errors.Error_List.Add(Lex);
+            //Error list
+            else
+            {
+                Errors.Error_List.Add(Lex);
+            }
         }
+
 
         bool isString(string Lex)
         {
-            Regex re = new Regex(@"\x22[^\x22]*\x22", RegexOptions.Compiled);
+            Regex re = new Regex(@"^\x22[^\x22]*\x22$", RegexOptions.Compiled);
             if (re.IsMatch(Lex) == true)
             {
                 return true;
@@ -252,11 +339,15 @@ namespace TINY_Compiler
 
         bool isIdentifier(string lex)
         {
-            bool isValid=true;
-            // Check if the lex is an identifier or not.
-            
-            return isValid;
+
+            Regex re = new Regex(@"^[a-zA-Z][a-zA-Z0-9]*$", RegexOptions.Compiled);
+            if (re.IsMatch(lex) == true)
+            {
+                return true;
+            }
+            return false;
         }
+
         bool isNumber(string lex)
         {
             Regex re = new Regex(@"^[0-9]+(\.[0-9]+)?$", RegexOptions.Compiled);
@@ -267,6 +358,27 @@ namespace TINY_Compiler
 
             return false;
         }
+        bool isReservedkeyword(string lex)
+        {
+            Regex re = new Regex(@"^(int|float|string|read|write|repeat|until|if|elseif|else|then|return|endl)$", RegexOptions.Compiled);
+            if (re.IsMatch(lex) == true)
+            {
+                return true;
+            }
+
+            return false;
+        }
+        bool isCommentStatment(string lex)
+        {
+            Regex re = new Regex(@"^/\*(.|\n)*\*/$", RegexOptions.Compiled);
+            if (re.IsMatch(lex) == true)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
         bool isOperator(string lex)
         {
             Regex re = new Regex(@"^(\+|\-|\*|\/|,|;|<|>|=|&&|\|\||<>|:=|\(|\)|\{|\})$" , RegexOptions.Compiled);
